c3ad46c1e78410feb0ca8675ac5b36cc299068b3
MrPreet <harpreet9629@gmail.com>
2023-02-13 13:28:52 +0530

    SEO added

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,10 @@
+import { DefaultSeo } from 'next-seo'
 import '../styles/globals.scss'
+import { DEFAULT_SEO } from '@app/constants'
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <DefaultSeo {...DEFAULT_SEO}/>
       <Component {...pageProps} />
     </>
   )
