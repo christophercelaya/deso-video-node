290fa1b9e2645384c805dc9b1a7a31d3882b65ed
MrPreet <harpreet9629@gmail.com>
2023-02-24 19:35:06 +0530

    Tracking code added

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -27,6 +27,24 @@ class MyDocument extends Document {
                     <link rel="preconnect" href="https://fonts.googleapis.com"/>
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
                     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;200;300;400;500;600;700;800;900&family=Oswald:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet" /> 
+                    <script
+                        async
+                        src={`https://www.googletagmanager.com/gtag/js?id=G-QYBSHWYH7B`}
+                    />
+
+                    <script
+                        // eslint-disable-next-line react/no-danger
+                        dangerouslySetInnerHTML={{
+                        __html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                            gtag('config', 'G-QYBSHWYH7B', {
+                            page_path: window.location.pathname,
+                            });
+                        `,
+                        }}
+                    />
                 </Head>
                 <body>
                     <Main />
