048daaf5b7b5626feb8ad3c376b00e34ec42b576
MrPreet <harpreet9629@gmail.com>
2023-02-24 19:52:26 +0530

    Bugs fixed

--- src/utils/functions/getProfileName.js
+++ src/utils/functions/getProfileName.js
@@ -2,6 +2,6 @@ import { getProfileExtraData } from "./getProfileExtraData";
 
 export const getProfileName = (profile) => {
     const channelExtra = getProfileExtraData(profile);
-    const displayName = channelExtra !== null ? channelExtra.DisplayName !== null ? channelExtra.DisplayName : profile.Username : profile.Username
+    const displayName = channelExtra !== null ? channelExtra.DisplayName !== '' ? channelExtra.DisplayName : profile.Username : profile.Username
     return displayName
 }
